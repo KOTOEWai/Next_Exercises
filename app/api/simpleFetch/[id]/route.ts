a76726b0311f54bcced1c590daa1f68668f48018
KOTOEWai <149601802+KOTOEWai@users.noreply.github.com>
2025-11-24 21:59:08 +0630

    Next

--- app/api/simpleFetch/[id]/route.ts
+++ app/api/simpleFetch/[id]/route.ts
@@ -1,18 +1,18 @@
 import { NextRequest } from "next/server";
 
 
-export async function GET(request:NextRequest, { params }: { params: { id: string } }) {
- const id = params.id;
+export async function GET(request:NextRequest, { params }: { params: Promise<{ id: string }> }) {
+ const id =  (await params).id;
 return Response.json({ message: "Hello from Next.js!" , id });
 }
 
-export async function PATCH(request:NextRequest, { params }: { params: { id: string } }) {
- const id = params.id;
+export async function PATCH(request:NextRequest, { params }: { params: Promise<{ id: string }> }) {
+ const id = (await params).id;
 return Response.json({ message: "PATCH request received!", id });
 }
 
-export async function DELETE(request:NextRequest, { params }: { params: { id: string } }) {
- const id = params.id;
+export async function DELETE(request:NextRequest, { params }: {params: Promise<{ id: string }> }) {
+ const id = (await params).id;
  const body = await request.json();
  const { confirm } = body;
 return Response.json({ message: "DELETE request received!", id });
